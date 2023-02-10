cb7b0818fad68cf73cbe15b0c90bde8d1f98bcde
RSKriegs <75480707+RSKriegs@users.noreply.github.com>
2023-02-10 14:56:24 +0100

    Update kafka-setup-k8s.sh

--- kafka/scripts/kafka-setup-k8s.sh
+++ kafka/scripts/kafka-setup-k8s.sh
@@ -1,5 +1,6 @@
+# for some reason this script stopped working in a meantime while deployed to Kafka pod that's why it's not used in a final deployment.
+# TODO: fix the issue above
 
-# blocks until kafka is reachable
 kafka-topics --bootstrap-server localhost:29092 --list
 
 echo -e 'Creating kafka topics'
